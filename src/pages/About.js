c1e1e5bb301282b6e4c5994ace06ed5a819c7f8f
Fonkarts <82459566+Fonkarts@users.noreply.github.com>
2022-07-09 14:24:51 +0200

    #1_Logique
    Upload de l'appli, logique terminée.

---
+++ src/pages/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>À propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis amet
+        sit dicta quidem laboriosam, vero in sunt sed, eaque molestias
+        voluptatibus nesciunt voluptatum eligendi voluptates, aliquid suscipit
+        modi unde doloribus officia ratione aliquam. Corrupti, omnis blanditiis
+        ad, culpa unde dolores voluptas nesciunt pariatur nihil corporis
+        inventore obcaecati tempora iure ea perferendis laboriosam dignissimos!
+        Debitis labore omnis cum reiciendis possimus. Veritatis incidunt ut,
+        consectetur distinctio amet quam iure alias? Eos in porro obcaecati,
+        repudiandae accusamus placeat alias ipsum iure minima. Repellendus
+        necessitatibus, corrupti architecto perspiciatis autem recusandae rem
+        fuga! Voluptate et itaque veritatis unde adipisci eaque iste ducimus,
+        mollitia dignissimos commodi.
+      </p>
+    </div>
+  );
+};
+
+export default About;
